c26ab57e4a71acf1d3dba8bad75dfb137f82afc0
Moritz <39065857+RealPotatoe@users.noreply.github.com>
2023-12-05 14:20:54 +0000

    ✨ Finished part one

--- day05/main.py
+++ day05/main.py
@@ -2,69 +2,80 @@ from collections import defaultdict
 
 
 class ConversionMap:
-    def __init__(self, destination_start: int, source_start: int, range: int) -> None:
-        self.destination_start = destination_start
-        self.source_start = source_start
-        self.range = range
+    def __init__(self, from_attr: str, to_attr: str) -> None:
+        self.from_attr = from_attr
+        self.to_attr = to_attr
+        self.rules = []
+
+    def add_rule(self, rule: "ConversionMapRule") -> None:
+        self.rules.append(rule)
+
+    def convert(self, seed: "Seed") -> None:
+        for rule in self.rules:
+            if seed.attributes[self.from_attr] in rule:
+                seed.attributes[self.to_attr] = rule.convert(
+                    seed.attributes[self.from_attr]
+                )
+                return
+
+        seed.attributes[self.to_attr] = seed.attributes[self.from_attr]
+
+
+class ConversionMapRule:
+    def __init__(self, dest_start: int, src_start: int, rng: int) -> None:
+        self.dest_start = dest_start
+        self.src_start = src_start
+        self.rng = rng
 
     def convert(self, seed: int) -> int:
-        if seed not in self:
-            raise ValueError(
-                f"Seed {seed} is not in range {self.source_start} - {self.source_start + self.range}"
-            )
-
-        return self.destination_start + (seed - self.source_start)
+        return self.dest_start + (seed - self.src_start)
 
     def __contains__(self, seed: int) -> bool:
-        return self.source_start <= seed < self.source_start + self.range
+        return self.src_start <= seed < self.src_start + self.rng
+
+
+class Seed:
+    def __init__(self, seed_num: int) -> None:
+        self.seed_num = seed_num
+        self.attributes = defaultdict(int)
+        self.attributes["seed"] = seed_num
 
 
 def main():
-    input_file_path = "day05/test_input.txt"
+    input_file_path = "day05/input.txt"
 
     with open(input_file_path, "r") as file:
         input_data = file.read().splitlines()
 
-    seeds = [int(seed) for seed in input_data[0].split(":")[1].strip().split()]
+    # Populate seeds
+    seeds = [
+        Seed(int(seed_num)) for seed_num in input_data[0].split(":")[1].strip().split()
+    ]
 
-    maps = defaultdict(list)
+    conversion_maps = []
+    input_data = input_data[2:]
 
-    input_data = input_data[3:]
-    stage = 0
-
+    # Populate conversion maps
     for line in input_data:
-        if line == "":
-            stage += 1
+        if not line:
             continue
 
         if line[0].isalpha():
+            from_attr, _, to_attr = line.split()[0].split("-")
+            conversion_maps.append(ConversionMap(from_attr, to_attr))
             continue
 
-        destination_start, source_start, range = [int(x) for x in line.split()]
-        maps[stage].append(ConversionMap(destination_start, source_start, range))
+        dest_start, src_start, rng = [int(x) for x in line.split()]
+        conversion_maps[-1].add_rule(ConversionMapRule(dest_start, src_start, rng))
 
-    conversions = defaultdict(int)
+    for conversion_map in conversion_maps:
+        for seed in seeds:
+            conversion_map.convert(seed)
 
-    for seed in seeds:
-        conversions[seed] = seed
-
-    print("Initial state:", conversions)
-
-    for stage in maps:
-        for seed in conversions:
-            conversion_found = False
-            for conversion_map in maps[stage]:
-                if seed in conversion_map:
-                    conversions[seed] = conversion_map.convert(seed)
-                    conversion_found = True
-                    print(f"Stage {stage}: {seed} -> {conversions[seed]}")
-                    break
-            if not conversion_found:
-                print(f"Stage {stage}: {seed} -> {seed} (no conversion found)")
-
-    minimum_seed = min(conversions, key=conversions.get)
-
-    print("Part one:", minimum_seed)
+    min_attribute = min(
+        [seed.attributes[conversion_maps[-1].to_attr] for seed in seeds]
+    )
+    print("Part 1:", min_attribute)
 
 
 if __name__ == "__main__":
