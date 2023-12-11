8b90aabb734d3ff6f903314c1c1b70d62a8ddc83
ThePotatoe <bengel_moritz@gmx.de>
2023-12-11 09:20:02 +0100

    🚧 WIP of Day11

---
+++ day11/main.py
@@ -0,0 +1,102 @@
+from typing import List, Set, Tuple
+
+
+def expand_empty_spaces(
+    space: List[List[str]], expansion_amount: int = 1
+) -> List[List[str]]:
+    where_to_insert = []
+    for row_id, row in enumerate(space):
+        if "#" in row:
+            continue
+        where_to_insert.append(row_id + 1)
+
+    for row_id in where_to_insert[::-1]:
+        for i in range(expansion_amount):
+            space.insert(row_id + i, ["."] * len(space[0]))
+
+    where_to_insert = []
+    for col_id, col in enumerate(zip(*space)):
+        if "#" in col:
+            continue
+        where_to_insert.append(col_id + 1)
+
+    for col_id in where_to_insert[::-1]:
+        for row in space:
+            for i in range(expansion_amount):
+                row.insert(col_id + i, ".")
+
+    return space
+
+
+def print_space(galaxy: List[List[str]]) -> None:
+    for row in galaxy:
+        print("".join(row))
+
+
+def get_distance(galaxy1: Tuple[int, int], galaxy2: Tuple[int, int]) -> int:
+    return abs(galaxy1[0] - galaxy2[0]) + abs(galaxy1[1] - galaxy2[1])
+
+
+def get_sum_of_distances(galaxies: List[Tuple[int, int]]) -> int:
+    sum_of_distances = 0
+    visited: Set[Tuple[int, int]] = set()
+    for galaxy1 in galaxies:
+        for galaxy2 in galaxies:
+            if galaxy1 == galaxy2:
+                continue
+            if (galaxy2, galaxy1) in visited:
+                continue
+            visited.add((galaxy1, galaxy2))
+            sum_of_distances += get_distance(galaxy1, galaxy2)
+
+    return sum_of_distances
+
+
+def main():
+    input_file = [
+        "...#......",
+        ".......#..",
+        "#.........",
+        "..........",
+        "......#...",
+        ".#........",
+        ".........#",
+        "..........",
+        ".......#..",
+        "#...#.....",
+    ]
+
+    # with open("day11/input.txt") as input_file:
+    #     input_file = input_file.readlines()
+
+    space = []
+    for line in input_file:
+        space.append(list(line.strip()))
+
+    expanded_space = expand_empty_spaces(space)
+
+    galaxies = [
+        (x, y)
+        for x in range(len(expanded_space))
+        for y in range(len(expanded_space[0]))
+        if expanded_space[x][y] == "#"
+    ]
+
+    sum_of_distances = get_sum_of_distances(galaxies)
+    print("Part one:", sum_of_distances)
+
+    older_expanded_space = expand_empty_spaces(space, 1000)
+
+    galaxies = [
+        (x, y)
+        for x in range(len(older_expanded_space))
+        for y in range(len(older_expanded_space[0]))
+        if older_expanded_space[x][y] == "#"
+    ]
+
+    sum_of_distances = get_sum_of_distances(galaxies)
+    print("Part two:", sum_of_distances)
+
+
+if __name__ == "__main__":
+    main()
