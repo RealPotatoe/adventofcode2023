2e82b7135ae32e72c8b37262e92ea0383fb4f000
ThePotatoe <bengel_moritz@gmx.de>
2023-12-12 19:49:28 +0100

    💡 Added explanation comments

--- day12/main.py
+++ day12/main.py
@@ -6,15 +6,24 @@ from typing import List
 def count_valid_patterns(springs: List[str], contiguous_broken: List[int]):
     @cache
     def dp(pos: int, broken_idx: int, result: int = 0) -> int:
+        # We have reached the end of the springs.
         if pos >= len(springs):
+            # If we have also reached the end of the contiguous broken list,
+            # then we have found a valid pattern. Otherwise, we have not.
             return broken_idx == len(contiguous_broken)
 
+        # Fork A: Continue with the next spring.
+        # If we land on a workign or unknown spring, we can continue to the next spring.
         if springs[pos] in ".?":
             result += dp(pos + 1, broken_idx)
 
+        # When we went through all the contiguous broken springs, we can return the result.
         if broken_idx == len(contiguous_broken):
             return result
 
+        # Fork B: Place the next block of contiguous broken springs, if possible.
+        # We check if we can place the next block of contiguous broken springs.
+        # If we can, we continue with the next spring after the contiguous broken springs.
         if (
             springs[pos] in "#?"
             and (
@@ -28,6 +37,7 @@ def count_valid_patterns(springs: List[str], contiguous_broken: List[int]):
         ):
             result += dp(pos + contiguous_broken[broken_idx] + 1, broken_idx + 1)
 
+        # Fially we return the result.
         return result
 
     return dp(0, 0)
