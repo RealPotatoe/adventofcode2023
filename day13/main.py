70ddfe01c1d2374b9f8f9372d118754a00b82aee
ThePotatoe <bengel_moritz@gmx.de>
2023-12-13 09:46:04 +0100

    ✨ Completed part 2 of day 13

--- day13/main.py
+++ day13/main.py
@@ -1,4 +1,4 @@
-from typing import Iterator, List, Tuple
+from typing import List
 
 
 def find_mirror_position(pattern: List[str]) -> int | None:
@@ -23,6 +23,29 @@ def find_mirror(pattern: List[str]) -> int | None:
     return find_mirror_position(flip(pattern))
 
 
+def find_smudged_mirror_position(pattern: List[str]) -> int | None:
+    for i in range(1, len(pattern)):
+        difference = 0
+        for n, row in enumerate(pattern):
+            if n == i and difference == 1:
+                return i
+            mirrored = i * 2 - 1 - n
+            if mirrored < len(pattern):
+                difference += sum(
+                    1 for char1, char2 in zip(row, pattern[mirrored]) if char1 != char2
+                )
+                if difference > 1:
+                    break
+
+
+def find_smudged_mirror(pattern: List[str]) -> int | None:
+    horizontal_mirror = find_smudged_mirror_position(pattern)
+    if horizontal_mirror:
+        return 100 * horizontal_mirror
+
+    return find_smudged_mirror_position(flip(pattern))
+
+
 def main():
     with open("day13/input.txt") as f:
         input_data = f.read().splitlines()
@@ -38,6 +61,9 @@ def main():
 
     sum_of_lines = sum([find_mirror(pattern) for pattern in patterns])
     print("Part one:", sum_of_lines)
+
+    sum_of_lines_smudged = sum([find_smudged_mirror(pattern) for pattern in patterns])
+    print("Part two:", sum_of_lines_smudged)
 
 
 if __name__ == "__main__":
